315e12f845702e4a59da550c15d61563aa06d5d4
pasilva1 <patrickataide.ti@gmail.com>
2026-03-23 17:15:20 -0300

    Enviando exercicios_2.js

---
+++ 02_condicionais/exercicios_2.js
@@ -0,0 +1,10 @@
+const idade = 17
+const nome = 'Patrick'
+
+if (idade >= 18) {
+    console.log('A idade do(a)', nome, 'é:',idade, ' Pode dirigir');   
+} else if (idade >= 17) {
+    console.log('A idade do(a)', nome, 'é:',idade, ' Pode procurar a auto-escola!');
+} else {
+    console.log('A idade do(a)', nome, 'é:',idade, ' Não pode dirigir');
+}
