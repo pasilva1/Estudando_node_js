5a8c617b1e8f4c462c9c66af327c0e38cfaac525
pasilva1 <patrickataide.ti@gmail.com>
2026-03-23 17:25:05 -0300

    Enviado exercicio uso_do_&&_AND.js

---
+++ 02_condicionais/uso_do_&&_AND.js
@@ -0,0 +1,9 @@
+const idade = 18
+const possuiCNH = true
+const nome = 'Patrick'
+
+if (idade >= 18 && possuiCNH === true) {
+    console.log('A idade do(a)', nome, 'é:',idade, ' Pode dirigir'); 
+} else {
+    console.log('A idade do(a)', nome, 'é:',idade, ' Não pode dirigir');
+}
